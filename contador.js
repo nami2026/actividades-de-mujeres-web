e8d545523bfc0f6f1ed0daf06275596f4cb05878
nami2026 <anybecerra1994@hotmail.com>
2024-08-15 10:37:17 -0500

    mensaje

---
+++ contador.js
@@ -0,0 +1,8 @@
+let contador=0;
+for(let numeros=1;numeros<=10;numeros++){
+    console.log(numeros);
+    if(numeros>5 && numeros<=10){
+        contador++;
+    }
+}
+console.log(contador);
---
+++ ejercicios 2.js
@@ -0,0 +1,3 @@
+let colores=["azul","rojo","morado","verde"];
+console.log(frutas[0]);
+console.log(frutas[3]);
